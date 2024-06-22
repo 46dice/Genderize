c52f0fd3086273bc71644ec14b6d02ab3ba08e05
Artur <artursa212@gmail.com>
2024-06-22 22:43:34 +0500

    fixed vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base:"/genderize",
-  plugins: [react()],
-})
+    base: '/Genderize',
+    plugins: [react()],
+});
